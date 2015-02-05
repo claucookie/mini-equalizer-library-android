81df88ea22787a0b5a238a231f9a0bcb79680599
Claudia Luque Fernández <claudia.luque.fernandez@gmail.com>
2015-02-05 09:17:25 +0100

    Uploaded project, Readme and sonatype configurations missing.

---
+++ app/src/main/java/es/claucookie/miniequalizersample/MainActivity.java
@@ -0,0 +1,72 @@
+package es.claucookie.miniequalizersample;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import es.claucookie.miniequalizerlibrary.EqualizerView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    EqualizerView equalizer;
+    Button toggleButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        equalizer = (EqualizerView) findViewById(R.id.equalizer_view);
+        toggleButton = (Button) findViewById(R.id.toggle_button);
+        showEqualizer();
+        initButton();
+    }
+
+    private void initButton() {
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleEqualizer();
+            }
+        });
+    }
+
+    private void toggleEqualizer() {
+        if (equalizer.isAnimating()) {
+            equalizer.stopBars();
+        } else {
+            equalizer.animateBars();
+        }
+    }
+
+    private void showEqualizer() {
+        equalizer.animateBars();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ library/src/main/java/es/claucookie/miniequalizerlibrary/EqualizerView.java
@@ -0,0 +1,152 @@
+package es.claucookie.miniequalizerlibrary;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.animation.LinearInterpolator;
+import android.widget.LinearLayout;
+
+/**
+ * Created by claucookie on 01/02/15.
+ */
+public class EqualizerView extends LinearLayout {
+
+    View musicBar1;
+    View musicBar2;
+    View musicBar3;
+
+    AnimatorSet playingSet;
+    AnimatorSet stopSet;
+    Boolean animating = false;
+
+    public EqualizerView(Context context) {
+        super(context);
+        initViews();
+    }
+
+    public EqualizerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initViews();
+    }
+
+    public EqualizerView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initViews();
+    }
+
+    private void initViews() {
+        LayoutInflater.from(getContext()).inflate(R.layout.view_equalizer, this, true);
+        musicBar1 = findViewById(R.id.music_bar1);
+        musicBar2 = findViewById(R.id.music_bar2);
+        musicBar3 = findViewById(R.id.music_bar3);
+        setPivots();
+    }
+
+
+    private void setPivots() {
+        musicBar1.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if (musicBar1.getHeight() > 0) {
+                    musicBar1.setPivotY(musicBar1.getHeight());
+                    if (Build.VERSION.SDK_INT >= 16) {
+                        musicBar1.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                    }
+                }
+
+            }
+        });
+        musicBar2.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if (musicBar2.getHeight() > 0) {
+                    musicBar2.setPivotY(musicBar2.getHeight());
+                    if (Build.VERSION.SDK_INT >= 16) {
+                        musicBar2.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                    }
+                }
+
+            }
+        });
+        musicBar3.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if (musicBar3.getHeight() > 0) {
+                    musicBar3.setPivotY(musicBar3.getHeight());
+                    if (Build.VERSION.SDK_INT >= 16) {
+                        musicBar3.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                    }
+                }
+
+            }
+        });
+    }
+
+    public void animateBars() {
+        animating = true;
+        if (playingSet == null) {
+            int duration = 3000;
+            ObjectAnimator scaleYbar1 = ObjectAnimator.ofFloat(musicBar1, "scaleY", 0.2f, 0.8f, 0.1f, 0.1f, 0.3f, 0.1f, 0.2f, 0.8f, 0.7f, 0.2f, 0.4f, 0.9f, 0.7f, 0.6f, 0.1f, 0.3f, 0.1f, 0.4f, 0.1f, 0.8f, 0.7f, 0.9f, 0.5f, 0.6f, 0.3f, 0.1f);
+            scaleYbar1.setRepeatCount(ValueAnimator.INFINITE);
+            ObjectAnimator scaleYbar2 = ObjectAnimator.ofFloat(musicBar2, "scaleY", 0.2f, 0.5f, 1.0f, 0.5f, 0.3f, 0.1f, 0.2f, 0.3f, 0.5f, 0.1f, 0.6f, 0.5f, 0.3f, 0.7f, 0.8f, 0.9f, 0.3f, 0.1f, 0.5f, 0.3f, 0.6f, 1.0f, 0.6f, 0.7f, 0.4f, 0.1f);
+            scaleYbar2.setRepeatCount(ValueAnimator.INFINITE);
+            ObjectAnimator scaleYbar3 = ObjectAnimator.ofFloat(musicBar3, "scaleY", 0.6f, 0.5f, 1.0f, 0.6f, 0.5f, 1.0f, 0.6f, 0.5f, 1.0f, 0.5f, 0.6f, 0.7f, 0.2f, 0.3f, 0.1f, 0.5f, 0.4f, 0.6f, 0.7f, 0.1f, 0.4f, 0.3f, 0.1f, 0.4f, 0.3f, 0.7f);
+            scaleYbar3.setRepeatCount(ValueAnimator.INFINITE);
+
+            playingSet = new AnimatorSet();
+            playingSet.playTogether(scaleYbar2, scaleYbar3, scaleYbar1);
+            playingSet.setDuration(duration);
+            playingSet.setInterpolator(new LinearInterpolator());
+            playingSet.start();
+
+        } else if (Build.VERSION.SDK_INT < 19) {
+            if (!playingSet.isStarted()) {
+                playingSet.start();
+            }
+        } else {
+            if (playingSet.isPaused()) {
+                playingSet.resume();
+            }
+        }
+
+    }
+
+    public void stopBars() {
+        animating = false;
+        if (playingSet != null && playingSet.isRunning() && playingSet.isStarted()) {
+            if (Build.VERSION.SDK_INT < 19) {
+                playingSet.end();
+            } else {
+                playingSet.pause();
+            }
+        }
+
+        if (stopSet == null) {
+            // Animate stopping bars
+            ObjectAnimator scaleY1 = ObjectAnimator.ofFloat(musicBar1, "scaleY", 0.1f);
+            ObjectAnimator scaleY2 = ObjectAnimator.ofFloat(musicBar2, "scaleY", 0.1f);
+            ObjectAnimator scaleY3 = ObjectAnimator.ofFloat(musicBar3, "scaleY", 0.1f);
+            stopSet = new AnimatorSet();
+            stopSet.playTogether(scaleY3, scaleY2, scaleY1);
+            stopSet.setDuration(200);
+            stopSet.start();
+        } else if (!stopSet.isStarted()) {
+            stopSet.start();
+        }
+    }
+
+    public Boolean isAnimating() {
+        return animating;
+    }
+
+    public void setAnimating(Boolean animating) {
+        this.animating = animating;
+    }
+
+}
